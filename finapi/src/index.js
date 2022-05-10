fcd43b4e0b406ed9596faa8b4684330c62e9c9e3
Pedro Estacio <pedronetoestacio@gmail.com>
2022-05-10 13:18:33 -0300

    Modulo 2

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -17,15 +17,36 @@ app.get('/account', (request, response) => {
     response.send(customers)
 })
 
+app.get('/statement', (request, response) => {
+    const { cpf } = request.headers;
+    console.log(cpf);
+
+    const customer = customers.find(customer => {customer.cpf === cpf});
+    if(!customer){
+        return response.status(400).json({
+            error: "Customer not found"
+        })
+    }
+
+    return response.json(customer.statement)
+})
+
 app.post('/account', (request, response) => {
 
     const {cpf, name} = request.body;
-    const id = uuidV4();
+
+    const customerAlreadyExists = customers.some((customer) => customer.cpf === cpf)
+
+    if(customerAlreadyExists){
+        return response.status(400).json({
+            error: "Customer Already Exists"
+        })
+    }
 
     customers.push({
         cpf,
         name,
-        id,
+        id: uuidV4(),
         statement: []
     })
 
